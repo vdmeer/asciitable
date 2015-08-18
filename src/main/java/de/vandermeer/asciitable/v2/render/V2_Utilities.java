b720a7cd11ee94c8379152a8e27e7194b13518df
vdmeer <sven@vandermeer.de>
2015-08-18 23:13:34 +0100

    added more java doc

--- src/main/java/de/vandermeer/asciitable/v2/render/V2_Utilities.java
+++ src/main/java/de/vandermeer/asciitable/v2/render/V2_Utilities.java
@@ -33,6 +33,40 @@ import de.vandermeer.asciitable.v2.themes.V2_RowTheme;
 public abstract class V2_Utilities {
 
 	/**
+	 * Creates a content array, that is an array with columns.
+	 * It will wrap lines and normalize the array.
+	 * @param columns the original columns
+	 * @param width the width of the columns
+	 * @param padding column padding
+	 * @return a fully processed and normalized content array
+	 */
+	public static final String[][] createContentArray(Object[] columns, int[] width, int[] padding){
+		String[][] ret = new String[width.length][];
+
+		int length = 0;
+		for(int i=0; i<columns.length; i++){
+			Object o = columns[i];
+			if(o==null){
+				length += width[i];
+				continue;
+			}
+			else{
+				length += width[i];
+			}
+			if(padding[i]>0){
+				length = length - padding[i]*2;
+			}
+			ret[i] = ArrayTransformations.WRAP_LINES(length, o);
+			length = 0;
+		}
+		//equal number of strings per column
+		ret = ArrayTransformations.NORMALISE_ARRAY(width.length, ret);
+		//flip so that each normalized array row is a table column
+		ret = ArrayTransformations.FLIP_ARRAY(ret);
+		return ret;
+	}
+
+	/**
 	 * Fixes top and bottom rules.
 	 * If the top of the table is a rule, the rule type will be changed to {@link RuleType#TOP}.
 	 * If the bottom of the table is a rule, the rule type will be changed to {@link RuleType#BOTTOM}.
@@ -45,144 +79,6 @@ public abstract class V2_Utilities {
 		if(table.getTable().get(table.getTable().size()-1) instanceof RuleRow){
 			((RuleRow)table.getTable().get(table.getTable().size()-1)).setRuleType(RuleType.BOTTOM);
 		}
-	}
-
-	/**
-	 * Returns a border character for given position and type from a theme.
-	 * @param pos position of the character: left, middle or right
-	 * @param type type of the character: all, content, down, up, none
-	 * @param tr theme for the character
-	 * @return the retrieved character from the theme
-	 */
-	public static char getChar(BorderPosition pos, BorderType type, V2_RowTheme tr){
-		switch(type){
-			case ALL:
-				switch(pos){
-					case LEFT:		return tr.getLeftBorder();
-					case MIDDLE:	return tr.getMidBorderAll();
-					case RIGHT:		return tr.getRightBorder();
-				}
-				break;
-			case CONTENT:
-				switch(pos){
-					case LEFT:		return tr.getLeftBorder();
-					case MIDDLE:	return tr.getMidBorderAll();
-					case RIGHT:		return tr.getRightBorder();
-				}
-				break;
-			case DOWN:
-				switch(pos){
-					case LEFT:		return tr.getLeftBorder();
-					case MIDDLE:	return tr.getMidBorderDown();
-					case RIGHT:		return tr.getRightBorder();
-				}
-				break;
-			case NONE:
-				return tr.getMid();
-			case UP:
-				switch(pos){
-					case LEFT:		return tr.getLeftBorder();
-					case MIDDLE:	return tr.getMidBorderUp();
-					case RIGHT:		return tr.getRightBorder();
-				}
-				break;
-		}
-		return 'X';
-	}
-
-	/**
-	 * Creates the border types for a content row.
-	 * @param ar an array of columns for the calculation
-	 * @param original the original row
-	 * @param colNumbers the number of columns in the table
-	 * @return array of border types, null if none could be created
-	 */
-	public static final BorderType[] getBorderTypes_ContentRow(String[] ar, ContentRow original, int colNumbers){
-		BorderType[] ret = null;
-
-		ret = new BorderType[colNumbers+1];
-
-		//left side
-		ret[0] = BorderType.CONTENT;
-		//right side
-		ret[colNumbers] = BorderType.CONTENT;
-
-		//in between
-		for(int i=0; i<ar.length; i++){
-			String content = ar[i];
-			if(content==null){
-				if(i==ar.length-1){
-					//a null in last column
-					ret[i+1] = BorderType.NONE;
-				}
-				else{
-					//standard null = span
-					ret[i+1] = BorderType.NONE;
-				}
-			}
-			else if("".equals(content)){
-				//empty column finishes span
-				ret[i+1] = BorderType.CONTENT;
-			}
-			else{
-				//all other columns finishing spans
-				ret[i+1] = BorderType.CONTENT;
-			}
-		}
-		return ret;
-	}
-
-	/**
-	 * Returns the border types for a top rule.
-	 * @param next the next row in the table
-	 * @param row the original row
-	 * @param colNumbers number of columns in the table
-	 * @return array of border types, null if none could be created
-	 */
-	public static final BorderType[] getBorderTypes_TopRule(ProcessedRow next, V2_Row row, int colNumbers){
-		BorderType[] ret = null;
-		if(!(row instanceof RuleRow)){
-			return ret;
-		}
-
-		RuleRow original = (RuleRow)row;
-
-		switch(original.getRuleType()){
-			case TOP:
-				ret = new BorderType[colNumbers+1];
-				for(int i=0; i<ret.length; i++){
-					ret[i] = BorderType.NONE;
-				}
-
-				if(next!=null){
-					BorderType[] relAdj = next.getBorderTypes();
-					if(relAdj!=null){
-						for(int i=0; i<relAdj.length; i++){
-						 	switch(relAdj[i]){
-								case NONE:
-									ret[i] = BorderType.NONE;
-									break;
-								case UP:
-									ret[i] = BorderType.DOWN;
-									break;
-								case ALL:
-									ret[i] = BorderType.DOWN;
-									break;
-								case DOWN:
-									ret[i] = BorderType.NONE;
-									break;
-								case CONTENT:
-									ret[i] = BorderType.DOWN;
-									break;
-							}
-						}
-					}
-				}
-				break;
-			default:
-				break;
-		}
-		return ret;
 	}
 
 	/**
@@ -234,6 +130,48 @@ public abstract class V2_Utilities {
 				break;
 			default:
 				break;
+		}
+		return ret;
+	}
+
+	/**
+	 * Creates the border types for a content row.
+	 * @param ar an array of columns for the calculation
+	 * @param original the original row
+	 * @param colNumbers the number of columns in the table
+	 * @return array of border types, null if none could be created
+	 */
+	public static final BorderType[] getBorderTypes_ContentRow(String[] ar, ContentRow original, int colNumbers){
+		BorderType[] ret = null;
+
+		ret = new BorderType[colNumbers+1];
+
+		//left side
+		ret[0] = BorderType.CONTENT;
+		//right side
+		ret[colNumbers] = BorderType.CONTENT;
+
+		//in between
+		for(int i=0; i<ar.length; i++){
+			String content = ar[i];
+			if(content==null){
+				if(i==ar.length-1){
+					//a null in last column
+					ret[i+1] = BorderType.NONE;
+				}
+				else{
+					//standard null = span
+					ret[i+1] = BorderType.NONE;
+				}
+			}
+			else if("".equals(content)){
+				//empty column finishes span
+				ret[i+1] = BorderType.CONTENT;
+			}
+			else{
+				//all other columns finishing spans
+				ret[i+1] = BorderType.CONTENT;
+			}
 		}
 		return ret;
 	}
@@ -318,36 +256,98 @@ public abstract class V2_Utilities {
 	}
 
 	/**
-	 * Creates a content array, that is an array with columns.
-	 * It will wrap lines and normalize the array.
-	 * @param columns the original columns
-	 * @param width the width of the columns
-	 * @param padding column padding
-	 * @return a fully processed and normalized content array
+	 * Returns the border types for a top rule.
+	 * @param next the next row in the table
+	 * @param row the original row
+	 * @param colNumbers number of columns in the table
+	 * @return array of border types, null if none could be created
 	 */
-	public static final String[][] createContentArray(Object[] columns, int[] width, int[] padding){
-		String[][] ret = new String[width.length][];
-
-		int length = 0;
-		for(int i=0; i<columns.length; i++){
-			Object o = columns[i];
-			if(o==null){
-				length += width[i];
-				continue;
-			}
-			else{
-				length += width[i];
-			}
-			if(padding[i]>0){
-				length = length - padding[i]*2;
-			}
-			ret[i] = ArrayTransformations.WRAP_LINES(length, o);
-			length = 0;
+	public static final BorderType[] getBorderTypes_TopRule(ProcessedRow next, V2_Row row, int colNumbers){
+		BorderType[] ret = null;
+		if(!(row instanceof RuleRow)){
+			return ret;
 		}
-		//equal number of strings per column
-		ret = ArrayTransformations.NORMALISE_ARRAY(width.length, ret);
-		//flip so that each normalized array row is a table column
-		ret = ArrayTransformations.FLIP_ARRAY(ret);
+
+		RuleRow original = (RuleRow)row;
+
+		switch(original.getRuleType()){
+			case TOP:
+				ret = new BorderType[colNumbers+1];
+				for(int i=0; i<ret.length; i++){
+					ret[i] = BorderType.NONE;
+				}
+
+				if(next!=null){
+					BorderType[] relAdj = next.getBorderTypes();
+					if(relAdj!=null){
+						for(int i=0; i<relAdj.length; i++){
+						 	switch(relAdj[i]){
+								case NONE:
+									ret[i] = BorderType.NONE;
+									break;
+								case UP:
+									ret[i] = BorderType.DOWN;
+									break;
+								case ALL:
+									ret[i] = BorderType.DOWN;
+									break;
+								case DOWN:
+									ret[i] = BorderType.NONE;
+									break;
+								case CONTENT:
+									ret[i] = BorderType.DOWN;
+									break;
+							}
+						}
+					}
+				}
+				break;
+			default:
+				break;
+		}
 		return ret;
+	}
+
+	/**
+	 * Returns a border character for given position and type from a theme.
+	 * @param pos position of the character: left, middle or right
+	 * @param type type of the character: all, content, down, up, none
+	 * @param tr theme for the character
+	 * @return the retrieved character from the theme
+	 */
+	public static char getChar(BorderPosition pos, BorderType type, V2_RowTheme tr){
+		switch(type){
+			case ALL:
+				switch(pos){
+					case LEFT:		return tr.getLeftBorder();
+					case MIDDLE:	return tr.getMidBorderAll();
+					case RIGHT:		return tr.getRightBorder();
+				}
+				break;
+			case CONTENT:
+				switch(pos){
+					case LEFT:		return tr.getLeftBorder();
+					case MIDDLE:	return tr.getMidBorderAll();
+					case RIGHT:		return tr.getRightBorder();
+				}
+				break;
+			case DOWN:
+				switch(pos){
+					case LEFT:		return tr.getLeftBorder();
+					case MIDDLE:	return tr.getMidBorderDown();
+					case RIGHT:		return tr.getRightBorder();
+				}
+				break;
+			case NONE:
+				return tr.getMid();
+			case UP:
+				switch(pos){
+					case LEFT:		return tr.getLeftBorder();
+					case MIDDLE:	return tr.getMidBorderUp();
+					case RIGHT:		return tr.getRightBorder();
+				}
+				break;
+		}
+		return 'X';
 	}
 }
--- src/main/java/de/vandermeer/asciitable/v2/render/package-info.java
+++ src/main/java/de/vandermeer/asciitable/v2/render/package-info.java
@@ -16,7 +16,67 @@
 /**
  * V2 classes to render a table.
  *
- *
+ *<p>
+ * Most of the classes and enumerates here should be used automatically by a V2 ASCII table.
+ * The main external class is the {@link de.vandermeer.asciitable.v2.render.V2_TableRenderer}, which is used to render a V2 ASCII table.
+ *</p>
+ * 
+ * 
+ * <h3>The Table Renderer Interface</h3>
+ * The {@link de.vandermeer.asciitable.v2.render.V2_TableRenderer} interface provides the base line for realizing a table renderer.
+ * It defines a number of methods to set the behavior of a renderer:
+ * <ul>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.V2_TableRenderer#setPaddingChar(char)} - sets the padding character for a renderer. This character is being used to pad all content rows (each line of them).</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.V2_TableRenderer#setWidth(de.vandermeer.asciitable.v2.render.width.V2_Width)} - sets the table width being calculated by the given width object.</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.V2_TableRenderer#setTheme(de.vandermeer.asciitable.v2.themes.V2_TableTheme)} - set the theme the renderer should use to render the table.</li>
+ * </ul>
+ * 
+ * 
+ * 
+ * <h3>The ASCII Table Renderer Implementation</h3>
+ * The class {@link de.vandermeer.asciitable.v2.render.V2_AsciiTableRenderer} implements a standard renderer.
+ * It is using the theme {@link de.vandermeer.asciitable.v2.themes.V2_E_TableThemes#PLAIN_7BIT} as default theme.
+ * 
+ * 
+ * 
+ * <h3>Define a border position</h3>
+ * The position of a border element in a table row is defined in {@link de.vandermeer.asciitable.v2.render.BorderPosition} as
+ * <ul>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.BorderPosition#LEFT} - a border on the left side of the row,</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.BorderPosition#RIGHT} - a border on the right side of the row,</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.BorderPosition#MIDDLE} - a border between left and right, in the middle.</li>
+ * </ul>
+ * 
+ * This border position assists a renderer to pick the right character from a row theme.
+ * 
+ * 
+ * <h3>Define a border type</h3>
+ * The type of a border element in a table row is defined in {@link de.vandermeer.asciitable.v2.render.BorderType} as
+ * <ul>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.BorderType#UP} - a border that goes up (linked to the previous row) only,</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.BorderType#DOWN} - a border that goes down (linked to the next row) only,</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.BorderType#ALL} - a border that goes up and down (linked to the previous and to the next row),</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.BorderType#CONTENT} - a border specific to a content row with special linkage to previous and next rows,</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.BorderType#NONE} - a border that goes neither up nor down nor is a content border.</li>
+ * </ul>
+ * 
+ * This border type assists a renderer to pick the right character from a row theme.
+ * 
+ * 
+ * 
+ * <h3>Utilities</h3>
+ * The class {@link de.vandermeer.asciitable.v2.render.V2_Utilities} implements a number of utility methods applicable to all (many) renderer implementations:
+ * <ul>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.V2_Utilities#createContentArray(Object[], int[], int[])} - creates a content array for a row that wraps over-size lines, normalizes the row array, and finally flips it for easy rendering,</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.V2_Utilities#fixTableRules(de.vandermeer.asciitable.v2.V2_AsciiTable)} - does set the right type for the top and bottom rule of a table,</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.V2_Utilities#getBorderTypes_BottomRule(ProcessedRow, de.vandermeer.asciitable.v2.row.V2_Row, int)} - returns the border types for a bottom rule,</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.V2_Utilities#getBorderTypes_ContentRow(String[], de.vandermeer.asciitable.v2.row.ContentRow, int)} - returns the border types for a content rule,</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.V2_Utilities#getBorderTypes_MidRule(ProcessedRow, ProcessedRow, de.vandermeer.asciitable.v2.row.V2_Row, int)} - returns the border types for a mid rule,</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.V2_Utilities#getBorderTypes_TopRule(ProcessedRow, de.vandermeer.asciitable.v2.row.V2_Row, int)} - returns the border types for a top rule,</li>
+ * 		<li>{@link de.vandermeer.asciitable.v2.render.V2_Utilities#getChar(BorderPosition, BorderType, de.vandermeer.asciitable.v2.themes.V2_RowTheme)} - returns the actual border character to be used from a row theme for a given border type and position.</li>
+ * </ul>
+ * 
+ * 
  * @author     Sven van der Meer &lt;vdmeer.sven@mykolab.com&gt;
  * @version    v0.2.0 build 150814 (14-Aug-15) for Java 1.7
  */
